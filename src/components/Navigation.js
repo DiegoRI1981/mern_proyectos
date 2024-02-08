84cb2d79b0ad63e178f51f5eb2d685b81f2fe7df
Diego Rodriguez <godiedri@gmail.com>
2024-02-08 16:54:54 +0100

    avance cleinte, barra de navegación

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,25 @@
-import React from 'react'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Form } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 export default function Navigation() {
     return (
-        <Navbar>
-            <h1>Navigation</h1>
+        <Navbar callapseOnSelect expand='lg' variant='dark' bg='secondary' >
+            <Navbar.Brand as={NavLink} to='/'>Task Manager</Navbar.Brand>
+            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+            <Navbar.Collapse id='responsive-navbar-nav'>
+                <Nav className='container text-center'>
+                    <Nav.Link as={NavLink} to='/projects'>Proyects</Nav.Link>
+                    <NavDropdown title='Admin'>
+                        <NavDropdown.Item as={NavLink} to='/admin/users'>Users</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav >
+                    <Nav.Link as={NavLink} to='/login'>Login</Nav.Link>
+                    <Nav.Link as={NavLink} to='/register'>Register</Nav.Link>
+                    <Nav.Link as={NavLink} to='/account'>My account</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+
         </Navbar>
     )
 }
